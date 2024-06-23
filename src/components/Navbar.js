707215d9d0c29c2064576ca1a856e37341d628c0
Gak1o <gakiotoni@gmail.com>
2024-06-23 23:55:09 +0300

    First changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../Styles/main.css";
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<h3>LOGO</h3>
+			<nav ref={navRef}>
+				<a href="/#">Home</a>
+				<a href="/#">Products</a>
+				<a href="/#">The Team</a>
+				<a href="/#">About Us</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
